a1a515e91f9f4d88de91045709e4e0c9ef0db1d9
dyu <david.yu.ftw@gmail.com>
2017-07-05 13:18:17 +0800

    prk docs, rename method

--- src/prk.ts
+++ src/prk.ts
@@ -1,3 +1,14 @@
+/**
+ * ```
+ * 
+ * message ParamRangeKey {
+ *   required bool desc = 1;
+ *   optional uint32 limit = 2;
+ *   optional bytes start_key = 3;
+ *   optional bytes parent_key = 4;
+ * }
+ * ```
+ */
 export interface ParamRangeKey {
     ['1']: boolean;
     ['2']?: number;
@@ -5,7 +16,24 @@ export interface ParamRangeKey {
     ['4']?: string;
 }
 
-export function $newParamRangeKey(desc: boolean, limit?: number, startKey?: string, parentKey?: string): ParamRangeKey {
+export const enum $ {
+    /** required: 1 */
+    desc = '1',
+    /** optional: 2 */
+    limit = '2',
+    /** optional: 3 */
+    startKey = '3',
+    /** optional: 4 */
+    parentKey = '4'
+}
+export const enum $0 {
+    desc = 1,
+    limit = 2,
+    startKey = 3,
+    parentKey = 4
+}
+
+export function $new(desc: boolean, limit?: number, startKey?: string, parentKey?: string): ParamRangeKey {
     return {
         '1': desc,
         '2': limit,
